457831d8ea2974749e70bea6900b2ff00ebabff1
LeviOP <35459268+LeviOP@users.noreply.github.com>
2019-04-05 13:45:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var today = new Date();
-var code = today.getHours();
+var code = today.getMinutes();
 document.addEventListener("DOMContentLoaded", function(){
     console.log("          ████████          \n        ██        ██        \n      ██▒▒          ██      \n    ██▒▒            ▒▒██    \n    ██▒▒            ▒▒██    \n  ██                  ▒▒██  \n  ██                  ▒▒██  \n██                        ██\n██                        ██\n██                      ▒▒██\n██▒▒                    ▒▒██\n  ██                  ▒▒██  \n  ██▒▒                ▒▒██  \n    ██▒▒              ██    \n      ████        ████      \n          ████████          ")
     console.log("To go have the next egg appear click the link below:")
