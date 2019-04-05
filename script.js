acdd799145cfb9eb90bc1dbda6f8bcb468c9155f
LeviOP <35459268+LeviOP@users.noreply.github.com>
2019-04-05 13:32:05 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+    document.addEventListener("DOMContentLoaded", function(){
+                                                              console.log("          ████████          \n        ██        ██        \n      ██▒▒          ██      \n    ██▒▒            ▒▒██    \n    ██▒▒            ▒▒██    \n  ██                  ▒▒██  \n  ██                  ▒▒██  \n██                        ██\n██                        ██\n██                      ▒▒██\n██▒▒                    ▒▒██\n  ██                  ▒▒██  \n  ██▒▒                ▒▒██  \n    ██▒▒              ██    \n      ████        ████      \n          ████████          ")
+                                                            });
