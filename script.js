bd134f6deae9bf21e4853ba9b628e7d904b2da3c
LeviOP <35459268+LeviOP@users.noreply.github.com>
2019-04-05 13:41:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ document.addEventListener("DOMContentLoaded", function(){
     console.log("          ████████          \n        ██        ██        \n      ██▒▒          ██      \n    ██▒▒            ▒▒██    \n    ██▒▒            ▒▒██    \n  ██                  ▒▒██  \n  ██                  ▒▒██  \n██                        ██\n██                        ██\n██                      ▒▒██\n██▒▒                    ▒▒██\n  ██                  ▒▒██  \n  ██▒▒                ▒▒██  \n    ██▒▒              ██    \n      ████        ████      \n          ████████          ")
     console.log("To go have the next egg appear click the link below:")
     console.log("https://easteregg.ml/?" + code)
+    if(window.location.href.indexOf("?" + code) > -1) {
+       alert("here is the next egg");
+    }
 });
 
