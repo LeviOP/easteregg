e27efc369b08fa6b4bebcf3c3d4900865ea3c186
LeviOP <35459268+LeviOP@users.noreply.github.com>
2019-04-23 10:20:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ var today = new Date();
 var minsCode = today.getMinutes();
 var hoursCode = today.getHours();
 var daysCode = today.getDay();
+var pre = document.getElementById("pre");
 document.addEventListener("DOMContentLoaded", function(){
     console.log("          ████████          \n        ██        ██        \n      ██▒▒          ██      \n    ██▒▒            ▒▒██    \n    ██▒▒            ▒▒██    \n  ██                  ▒▒██  \n  ██                  ▒▒██  \n██                        ██\n██                        ██\n██                      ▒▒██\n██▒▒                    ▒▒██\n  ██                  ▒▒██  \n  ██▒▒                ▒▒██  \n    ██▒▒              ██    \n      ████        ████      \n          ████████          ")
     console.log("To go have the next egg appear click the link below:")
     console.log("https://easteregg.ml/?code=" + daysCode + hoursCode + minsCode)
     if(window.location.href.indexOf("?code=" + daysCode + hoursCode + minsCode) > -1) {
        alert("Here is the next egg. (This message will always appear; if you ever don't see it, you will have to go through the whole system again, starting form the beginning.");
+       pre.innerHTML("oof");
     }
 });
 
