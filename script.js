c84312f404afade72ea7be2675f844e5b1c916ab
LeviOP <35459268+LeviOP@users.noreply.github.com>
2019-04-23 11:24:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ document.addEventListener("DOMContentLoaded", function(){
     console.log("https://easteregg.ml/?code=" + daysCode + hoursCode + minsCode)
     if(window.location.href.indexOf("?code=" + daysCode + hoursCode + minsCode) > -1) {
        alert("Here is the next egg. (This message will always appear; if you ever don't see it, you will have to go through the whole system again, starting form the beginning.");
-       document.getElementById("pre").innerHTML = "oof";
+       documnet.getElementById("pre").style.color = "White"
+       document.getElementById("pre").innerHTML = "          ████████          \n        ██        ██        \n      ██▒▒          ██      \n    ██▒▒            ▒▒██    \n    ██▒▒            ▒▒██    \n  ██                  ▒▒██  \n  ██                  ▒▒██  \n██                        ██\n██                        ██\n██                      ▒▒██\n██▒▒                    ▒▒██\n  ██                  ▒▒██  \n  ██▒▒                ▒▒██  \n    ██▒▒              ██    \n      ████        ████      \n          ████████          ";
     }
 });
 
